6d31366936be999131e7de30da3c089c9f7f568a
saiKumarKulai <118827147+saiKumarKulai@users.noreply.github.com>
2023-07-02 16:38:00 +0530

    Update script.bash

--- script.bash
+++ script.bash
@@ -5,11 +5,11 @@ git config --global user.email "ksai96py@gmai.com"
 git config --global user.name "saiKumarKulai"
 git clone "https://${git_token}github.com/SaiLuckyAss/learnGitHubActions"
 cd learnGitHubActions
-git checkout -b branch_from_yml
+git checkout -b new_bran
 echo "Heleleo" > xyz.txt
 git add --all
 git commit -m "Comma"
-git push -u origin branch_from_yml
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $git_token" https://api.github.com/repos/SaiLuckyAss/dummy/pulls -d '{"title":"CICD: Add git act","body":"Adding","head":"branch_from_yml","base":"main"}'  
+git push -u origin new_bran
+curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $git_token" https://api.github.com/repos/SaiLuckyAss/dummy/pulls -d '{"title":"CICD: Add git act","body":"Adding","head":"new_bran","base":"main"}'  
 
 cd ../
