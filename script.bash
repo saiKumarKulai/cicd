6c82cb8de4440b0b688266bea3dbbc6b44437157
saiKumarKulai <118827147+saiKumarKulai@users.noreply.github.com>
2023-07-02 15:57:47 +0530

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,15 @@
+git conifg --global user.email "ksai96py@gmai.com"
+git config --global user.name "saiKumarKulai"
+git clone "https://${git_token}github.com/SaiLuckyAss/learnGitHubActions"
+cd learnGitHubActions
+git checkout -b branch_from_yml
+echo "Heleleo" > xyz.txt
+git add --all
+git commit -m "Comma"
+git push -u origin branch_from_yml
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${git_token} \
+  https://api.github.com/repos/SaiLuckyAss/learnGitHubActions/pulls \
+  -d {"title":"CI","body":"Body","head":"branch_from_yml","base":"main"}
