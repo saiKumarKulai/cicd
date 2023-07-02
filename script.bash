46f2ec801e86f4a93a6333562cc3bcff1bb8f919
saiKumarKulai <118827147+saiKumarKulai@users.noreply.github.com>
2023-07-02 16:33:51 +0530

    Update script.bash

--- script.bash
+++ script.bash
@@ -10,11 +10,6 @@ echo "Heleleo" > xyz.txt
 git add --all
 git commit -m "Comma"
 git push -u origin branch_from_yml
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${git_token}" \
-  https://api.github.com/repos/SaiLuckyAss/learnGitHubActions/pulls \
-  -d '{"title":"CI","body":"Body","head":"'"branch_from_yml"'","base":"'"main"'"}'
+$  curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $git_token" https://api.github.com/repos/SaiLuckyAss/dummy/pulls -d '{"title":"CICD: Add git act","body":"Adding","head":"branch_from_yml","base":"main"}'  
 
 cd ../
